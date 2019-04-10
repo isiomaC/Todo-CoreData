95fa65ee49c49a6f9005c703b44c15c687b8dc8f
Anofienam Isioma <isioma.chuck@gmail.com>
2019-04-10 13:10:25 +0100

    Implemented CRUD, search using UISearchBar

--- Todo-CoreData/Controller/TodoListViewController.swift
+++ Todo-CoreData/Controller/TodoListViewController.swift
@@ -24,7 +24,7 @@ class TodoListViewController: UITableViewController {
         print(dataFilePath)
 
         loadItems()
-        
+        loadItems()
     }
     
     //MARK - Rable view datasource methods
